40d8c29254dbb3aa84b45069523778a912d3769c
Kovalchuk63 <vov4uk4763@gmail.com>
2023-09-09 17:24:40 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import Notiflix from "notiflix";
-import { PicAPI } from "./PBPic";
-import SimpleLightBox from 'simplelightbox';
+import Notiflix from 'notiflix';
+import { PicAPI } from './PBPic';
+
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -8,129 +9,98 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: '250',
 });
 
-const picAPI = new PicAPI();
+const picApi = new PicAPI();
 
 const refs = {
-  blockform: document.querySelector('#search-form'),
-  btnload: document.querySelector('.load-more'),
-  picset: document.querySelector('.gallery')
-};
+    formBox: document.querySelector('#search-form'),
+    picSet: document.querySelector('.gallery'),
+    btnMore: document.querySelector('.load-more'),
+  };
 
-refs.blockform.addEventListener('submit', onSubmit);
-refs.btnload.addEventListener('click', onBtn);
-window.addEventListener('scroll', onScroll);
+  refs.formBox.addEventListener('submit', onSubmit);
+  refs.btnMore.addEventListener('click', onBtn);
 
-async function onBtn(evt) {
-  try {
-    const data = await picApi.getPic();
-    renderPic(data.hits);
-    if (picApi.PAGE >= picApi.TOTAL_PAGES) {
-      Notiflix.Notify.warning(
-        "We're sorry, but you've reached the end of search results."
-      );
-      refs.loadMoreBtn.disabled = true;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-
-async function onScroll() {
-  const scrollPosition = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
-
-   /* прокрутив сторінку до певного порогу  80% */
-  if (scrollPosition + windowHeight >= documentHeight * 0.8) {
+  async function onBtn(evt) {
     try {
-      const data = await picAPI.getPic();
+      const data = await picApi.getPic();
       renderPic(data.hits);
-
-      if (picAPI.PAGE >= picAPI.TOTAL_PAGES) {
-        showEndOfResultsMessage();
-        window.removeEventListener('scroll', onScroll); 
+      if (picApi.PAGE >= picApi.TOTAL_PAGES) {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+        refs.btnMore.disabled = true;
       }
     } catch (error) {
-      handleAPIError(error);
+      console.error(error);
     }
   }
-}
 
-async function onSubmit(evt) {
-  evt.preventDefault();
-  const query = evt.target.searchQuery.value.trim();
-
-  if (query === '') {
-    Notiflix.Notify.failure('Please, enter a search query.');
-    return;
-  }
-
-  refs.picset.innerHTML = '';
-  picAPI.resetPage();
-  picAPI.QUERY = query;
-
-  try { 
-    const data = await picAPI.getPic();
-
-    if (data.hits.length === 0) {
-      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    } else {
-      enableLoadMoreButton();
+  async function onSubmit(evt) {
+    evt.preventDefault();
+    const query = e.target.elements.searchQuery.value;
+  
+    if (query.trim() === '') {
+      Notiflix.Notify.failure('Please enter a search query.');
+      return;
+    }
+  
+    refs.picSet.innerHTML = '';
+  
+    picApi.resetPage();
+    picApi.QUERY = query;
+    try {
+      const data = await picApi.getPic();
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      refs.btnMore.disabled = false;
+  
+      if (data.hits.length < picApi.PerPage) {
+        refs.btnMore.disabled = true;
+      }
+  
       renderPic(data.hits);
       showTotalHits(data.totalHits);
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    handleAPIError(error);
   }
-}
 
+  function markupHits(hits) {
+    return hits
+      .map(element => {
+        return `<a class="photo-card" href="${element.largeImageURL}">
+         
+            <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+          <b>Likes: ${element.likes}</b>
+              </p>
+              <p class="info-item">
+          <b>Views: ${element.views}</b>
+              </p>
+              <p class="info-item">
+          <b>Comments: ${element.comments}</b>
+              </p>
+              <p class="info-item">
+          <b>Downloads: ${element.downloads}</b>
+              </p>
+            </div>
+         
+        </a>`;
+      })
+      .join('');
+  }
 
-
-
-function renderPic(hits) {
-  const markup = hits.map(element => {
-    return `<a class="photo-card" href="${element.largeImageURL}">
-              <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
-              <div class="info">
-                  <p class="info-item"><b>Likes: ${element.likes}</b></p>
-                  <p class="info-item"><b>Views: ${element.views}</b></p>
-                  <p class="info-item"><b>Comments: ${element.comments}</b></p>
-                  <p class="info-item"><b>Downloads: ${element.downloads}</b></p>
-              </div>
-            </a>`;
-  }).join('');
-
-  refs.picset.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
-}
-
-function showTotalHits(totalHits) {
-  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-}
-
-function showErrorMessage(message) {
-  Notiflix.Notify.failure(message);
-}
-
-function showEndOfResultsMessage() {
-  Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`);
-  disableLoadMoreButton();
-}
-
-function handleAPIError(error) {
-  console.error(error);
-  showErrorMessage('An error occurred while fetching data. Please try again later.');
-}
-
-function enableLoadMoreButton() {
-  refs.btnload.disabled = false;
-}
-
-function disableLoadMoreButton() {
-  refs.btnload.disabled = true;
-}
-
-
-
+  function renderPic(hits) {
+    const markup = markupHits(hits);
+    refs.picSet.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+  }
+  
+  function showTotalHits(totalHits) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
